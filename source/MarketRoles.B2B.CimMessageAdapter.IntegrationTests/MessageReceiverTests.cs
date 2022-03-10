3cfd963ec6d7e9dbfbe3c35eb02989c8b490988a
Kristian F. Thomsen <kft@energinet.dk>
2022-03-10 12:30:54 +0100

    Incoming messages is stored in message store

---
+++ source/MarketRoles.B2B.CimMessageAdapter.IntegrationTests/MessageReceiverTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MarketRoles.B2B.CimMessageAdapter.IntegrationTests
+{
+#pragma warning disable
+    public class MessageReceiverTests
+    {
+        [Fact]
+        public async Task Message_must_be_stored()
+        {
+            var message = new RawMessage("xml");
+            var messageStore = new MessageStore();
+            var messageReceiver = new MessageReceiver(messageStore);
+            await messageReceiver.Receive(message).ConfigureAwait(false);
+
+            Assert.Contains(message, messageStore.Messages);
+        }
+    }
+
+    public class RawMessage
+    {
+        private readonly string _xml;
+
+        public RawMessage(string xml)
+        {
+            _xml = xml;
+        }
+    }
+
+    public class MessageStore
+    {
+        public List<RawMessage> Messages { get; } = new();
+
+        public Task SaveAsync(RawMessage message)
+        {
+            if (message == null) throw new ArgumentNullException(nameof(message));
+            Messages.Add(message);
+            return Task.CompletedTask;
+        }
+    }
+
+    public class MessageReceiver
+    {
+        private readonly MessageStore _storage;
+
+        public MessageReceiver(MessageStore storage)
+        {
+            _storage = storage;
+        }
+
+        public Task Receive(RawMessage message)
+        {
+            if (message == null) throw new ArgumentNullException(nameof(message));
+            return _storage.SaveAsync(message);
+        }
+    }
+}
