feb9551ea9f99c1475b47a036b24a64f3edb1c05
Kristian F. Thomsen <kft@energinet.dk>
2022-03-25 10:46:20 +0100

    Transaction is accepted and registered

---
+++ source/B2B.Transactions.Tests/TransactionHandlingTests.cs
@@ -0,0 +1,77 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using B2B.CimMessageAdapter.Messages;
+using B2B.CimMessageAdapter.Transactions;
+using Xunit;
+
+namespace B2B.Transactions.Tests
+{
+#pragma warning disable
+    public class TransactionHandlingTests
+    {
+        [Fact]
+        public void Transaction_is_registered()
+        {
+            var repository = new TransactionRepository();
+            var transaction = B2BTransaction.Create(
+                new MessageHeader("fake", "fake", "fake", "fake", "fake", "somedate", "fake"),
+                new MarketActivityRecord()
+                {
+                    BalanceResponsibleId = "fake",
+                    Id = "fake",
+                    ConsumerId = "fake",
+                    ConsumerName = "fake",
+                    EffectiveDate = "fake",
+                    EnergySupplierId = "fake",
+                    MarketEvaluationPointId = "fake",
+                });
+
+            var acceptedTransaction = new AcceptedTransaction(transaction.MarketActivityRecord.Id);
+            repository.Add(acceptedTransaction);
+
+            var savedTransaction = repository.Get(acceptedTransaction.TransactionId);
+            Assert.NotNull(savedTransaction);
+        }
+    }
+
+    public class TransactionRepository
+    {
+        private readonly List<AcceptedTransaction> _transactions = new();
+
+        public void Add(AcceptedTransaction acceptedTransaction)
+        {
+            _transactions.Add(acceptedTransaction);
+        }
+
+        public AcceptedTransaction Get(string transactionId)
+        {
+            return _transactions.FirstOrDefault(transaction =>
+                transaction.TransactionId.Equals(transactionId, StringComparison.OrdinalIgnoreCase));
+        }
+    }
+
+    public class AcceptedTransaction
+    {
+        public AcceptedTransaction(string transactionId)
+        {
+            TransactionId = transactionId;
+        }
+
+        public string TransactionId { get; }
+    }
+}
