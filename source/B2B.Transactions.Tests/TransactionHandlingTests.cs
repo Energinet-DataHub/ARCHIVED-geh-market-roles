1a91ab3fe6adf36aa485e0e6bb544bb01be40da2
Kristian F. Thomsen <kft@energinet.dk>
2022-03-25 14:06:27 +0100

    Assert id and type

--- source/B2B.Transactions.Tests/TransactionHandlingTests.cs
+++ source/B2B.Transactions.Tests/TransactionHandlingTests.cs
@@ -15,6 +15,9 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
+using System.Xml;
+using System.Xml.Linq;
 using B2B.CimMessageAdapter.Messages;
 using B2B.CimMessageAdapter.Transactions;
 using Xunit;
@@ -42,10 +45,13 @@ namespace B2B.Transactions.Tests
         [Fact]
         public void Accept_message_is_sent_to_sender_when_transaction_is_accepted()
         {
-            RegisterTransaction(CreateTransaction());
+            var transaction = CreateTransaction();
+            RegisterTransaction(transaction);
 
             var acceptMessage = _outgoingMessages.Messages.FirstOrDefault();
-            Assert.Equal("ConfirmRequestChangeOfSupplier_MarketDocument", acceptMessage.DocumentType);
+            Assert.NotNull(GetValue(acceptMessage, "mRID"));
+            AssertHasValue(acceptMessage, "type", "414");
+
             Assert.NotNull(acceptMessage);
         }
 
@@ -56,9 +62,35 @@ namespace B2B.Transactions.Tests
             var acceptedTransaction = new AcceptedTransaction(transaction.MarketActivityRecord.Id);
             repository.Add(acceptedTransaction);
 
+            var messageBody = new StringBuilder();
+            var settings = new XmlWriterSettings() { OmitXmlDeclaration = true };
+
+            using var writer = XmlWriter.Create(messageBody, settings);
+            writer.WriteStartDocument();
+            writer.WriteStartElement("cim", "ConfirmRequestChangeOfSupplier_MarketDocument", "urn:ediel.org:structure:confirmrequestchangeofsupplier:0:1");
+            writer.WriteAttributeString("xmlns", "xsi", null, "http://www.w3.org/2001/XMLSchema-instance");
+            writer.WriteAttributeString("xsi", "schemaLocation", null, "urn:ediel.org:structure:confirmrequestchangeofsupplier:0:1 urn-ediel-org-structure-confirmrequestchangeofsupplier-0-1.xsd");
+            writer.WriteElementString("mRID", null, Guid.NewGuid().ToString());
+            writer.WriteElementString("type", null, "414");
+            writer.WriteEndElement();
+            writer.Close();
+
+
+
+                // <cim:type>414</cim:type>
+                // <cim:process.processType>E03</cim:process.processType>
+                // <cim:businessSector.type>23</cim:businessSector.type>
+                // <cim:sender_MarketParticipant.mRID codingScheme="A10">5799999933318</cim:sender_MarketParticipant.mRID>
+                // <cim:sender_MarketParticipant.marketRole.type>DDZ</cim:sender_MarketParticipant.marketRole.type>
+                // <cim:receiver_MarketParticipant.mRID codingScheme="A10">5790001330552</cim:receiver_MarketParticipant.mRID>
+                // <cim:receiver_MarketParticipant.marketRole.type>DDQ</cim:receiver_MarketParticipant.marketRole.type>
+                // <cim:createdDateTime>2022-09-07T09:30:47Z</cim:createdDateTime>
+                // <cim:reason.code>A01</cim:reason.code>
+
+
             _outgoingMessages.Add(new AcceptMessage()
             {
-                DocumentType = "ConfirmRequestChangeOfSupplier_MarketDocument",
+                MessagePayload = messageBody.ToString(),
             });
         }
 
@@ -77,6 +109,19 @@ namespace B2B.Transactions.Tests
                     MarketEvaluationPointId = "fake",
                 });
         }
+
+        private static void AssertHasValue(AcceptMessage message, string elementName, string expectedValue)
+        {
+            Assert.Equal(expectedValue, GetValue(message, elementName));
+        }
+
+        private static string? GetValue(AcceptMessage message, string elementName)
+        {
+            var document = XDocument.Parse(message.MessagePayload);
+            XNamespace ns = "urn:ediel.org:structure:confirmrequestchangeofsupplier:0:1";
+
+            return document?.Element(ns + "ConfirmRequestChangeOfSupplier_MarketDocument").Element(elementName).Value;
+        }
     }
 
     public class MessageQueue
@@ -91,7 +136,7 @@ namespace B2B.Transactions.Tests
 
     public class AcceptMessage
     {
-        public string DocumentType { get; init; }
+        public string MessagePayload { get; init; }
     }
 
     public class TransactionRepository
