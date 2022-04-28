5e690f58b4433570c38bd013c802e953b213a701
Kristian F. Thomsen <kft@energinet.dk>
2022-04-28 10:56:52 +0200

    Move to new home

--- source/B2B.Transactions/IncomingMessages/RequestChangeOfSupplier/MarketActivityRecord.cs
+++ source/B2B.Transactions/IncomingMessages/RequestChangeOfSupplier/MarketActivityRecord.cs
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-namespace B2B.Transactions.IncomingMessages
+namespace B2B.Transactions.IncomingMessages.RequestChangeOfSupplier
 {
     public class MarketActivityRecord
     {
