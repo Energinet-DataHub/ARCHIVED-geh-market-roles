8506119b00444d3e9fb424b15b2bed4aef3dbd10
Kristian F. Thomsen <kft@energinet.dk>
2022-05-05 12:28:11 +0200

    Add API project

---
+++ source/Processing.Api/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Processing.Api.Configuration;
+using Processing.Infrastructure.Configuration;
+
+namespace Processing.Api
+{
+    public static class Program
+    {
+        public static async Task Main()
+        {
+            var runtime = RuntimeEnvironment.Default;
+
+            var host = ConfigureHost(runtime);
+
+            await host.RunAsync().ConfigureAwait(false);
+        }
+
+        private static IHost ConfigureHost(RuntimeEnvironment runtime)
+        {
+            return new HostBuilder()
+                .ConfigureFunctionsWorkerDefaults(worker =>
+                {
+                })
+                .ConfigureServices(services =>
+                {
+                    CompositionRoot.Initialize(services);
+                })
+                .Build();
+        }
+    }
+}
