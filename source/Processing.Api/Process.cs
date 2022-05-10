95d025a04060e992fd1d0e9f988a72358aa20741
Daniel Pethjen <daniel@pethjen.dk>
2022-05-10 10:19:26 +0200

    Add process function

---
+++ source/Processing.Api/Process.cs
@@ -0,0 +1,43 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+
+namespace Processing.Api;
+
+public class Process
+{
+    private readonly ILogger<Process> _logger;
+
+    public Process(
+        ILogger<Process> logger)
+    {
+        _logger = logger;
+    }
+
+    [Function("Process")]
+    public Task<HttpResponseData> RunAsync(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "post")]
+        HttpRequestData request)
+    {
+        _logger.LogInformation($"Received {nameof(Process)} request");
+
+        var response = request.CreateResponse(HttpStatusCode.OK);
+        return Task.FromResult<HttpResponseData>(response);
+    }
+}
