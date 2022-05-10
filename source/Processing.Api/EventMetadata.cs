56525884408b6b66ea0136dc75a2e239f2ad838d
Kristian F. Thomsen <kft@energinet.dk>
2022-05-10 09:06:59 +0200

    Rename project (#970)
    * Remove queue listener function
    
    * Rename project

--- source/Processing.Api/EventMetadata.cs
+++ source/Processing.Api/EventMetadata.cs
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-namespace Energinet.DataHub.MarketRoles.EntryPoints.Processing
+namespace Processing.Api
 {
         public class EventMetadata
         {
