8b6f2bf3ff38de7557ba6bdf247384e36ec33961
Daniel Pethjen <daniel@pethjen.dk>
2022-05-10 11:05:27 +0200

    expand dto and function

--- source/Processing.Api/MoveInProcess.cs
+++ source/Processing.Api/MoveInProcess.cs
@@ -12,32 +12,47 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+using System;
+using System.IO;
 using System.Net;
+using System.Text;
 using System.Threading.Tasks;
+using MediatR;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
+using Processing.Application.ChangeOfSupplier;
+using Processing.Infrastructure.Serialization;
 
 namespace Processing.Api;
 
 public class MoveInProcess
 {
     private readonly ILogger<MoveInProcess> _logger;
+    private readonly IJsonSerializer _jsonSerializer;
+    private readonly IMediator _mediator;
 
     public MoveInProcess(
-        ILogger<MoveInProcess> logger)
+        ILogger<MoveInProcess> logger,
+        IJsonSerializer jsonSerializer,
+        IMediator mediator)
     {
         _logger = logger;
+        _jsonSerializer = jsonSerializer;
+        _mediator = mediator;
     }
 
     [Function("MoveInProcess")]
-    public Task<HttpResponseData> RunAsync(
+    public async Task<HttpResponseData> RunAsync(
         [HttpTrigger(AuthorizationLevel.Anonymous, "post")]
         HttpRequestData request)
     {
         _logger.LogInformation($"Received {nameof(MoveInProcess)} request");
 
+        var command = _jsonSerializer.Deserialize<RequestChangeOfSupplier>(request?.Body.ToString() ?? throw new InvalidOperationException());
+        var responseBody = _jsonSerializer.Serialize(await _mediator.Send(command).ConfigureAwait(false));
         var response = request.CreateResponse(HttpStatusCode.OK);
-        return Task.FromResult<HttpResponseData>(response);
+        response.Body = new MemoryStream(Encoding.UTF8.GetBytes(responseBody));
+        return await Task.FromResult(response).ConfigureAwait(false);
     }
 }
