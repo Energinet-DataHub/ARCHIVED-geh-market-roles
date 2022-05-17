a1dab370bfa9e931a59c170c14616e47d6e1c071
Kristian F. Thomsen <kft@energinet.dk>
2022-05-17 10:19:21 +0200

    Refactor tests

--- source/Processing.Tests/Domain/Common/EffectiveDateTests.cs
+++ source/Processing.Tests/Domain/Common/EffectiveDateTests.cs
@@ -13,7 +13,6 @@
 // limitations under the License.
 
 using System;
-using System.Globalization;
 using Processing.Domain.Common;
 using Xunit;
 using Xunit.Categories;
@@ -24,18 +23,11 @@ namespace Processing.Tests.Domain.Common
     public class EffectiveDateTests
     {
         [Theory]
-        [InlineData("2021-06-01T22:00:00Z", true)]
-        [InlineData("2021-06-01T10:00:00Z", true)]
         [InlineData("2021-12-30T23:00:00Z", true)]
         [InlineData("2021-06-01T22:00:00.000Z", false)]
-        [InlineData("2021-06-01T22:00:00.100Z", false)]
-        [InlineData("2021-06-01T23:01:00Z", true)]
-        [InlineData("2021-06-01T00:00:00Z", true)]
-        [InlineData("2021-06-01T00:00:00.000Z", false)]
-        [InlineData("2021-09-25T22:00:00Z", true)]
         [InlineData("2021-06-01", false)]
         [InlineData("Not a date", false)]
-        public void Date_format_must_be_able_to_handle_daylight_savings(string dateString, bool isValid)
+        public void Must_be_utc_format(string dateString, bool isValid)
         {
             var result = EffectiveDate.CheckRules(dateString);
 
@@ -45,7 +37,7 @@ namespace Processing.Tests.Domain.Common
         [Fact]
         public void Create_should_succeed_when_date_format_is_valid()
         {
-            var dateString = DaylightSavingsString(new DateTime(2021, 6, 1));
+            var dateString = "2021-12-30T23:00:00Z";
             var effectiveDate = EffectiveDate.Create(dateString);
 
             Assert.NotNull(effectiveDate);
@@ -62,49 +54,10 @@ namespace Processing.Tests.Domain.Common
         [Fact]
         public void Create_should_succeed_when_date_is_passed()
         {
-            var date = DaylightSavingsAdjusted(DateTime.Now);
+            var date = DateTime.Now;
             var effectiveDate = EffectiveDate.Create(date);
 
             Assert.NotNull(effectiveDate);
-            Assert.Equal(date, effectiveDate.DateInUtc.ToDateTimeUtc());
-        }
-
-        private static DateTime DaylightSavingsAdjusted(DateTime date)
-        {
-            var info = TimeZoneInfo.FindSystemTimeZoneById("W. Europe Standard Time");
-            var isDaylightSavingTime = info.IsDaylightSavingTime(date);
-
-            return new DateTime(
-                date.Year,
-                date.Month,
-                date.Day,
-                isDaylightSavingTime ? 22 : 23,
-                0,
-                0);
-        }
-
-        private static string DaylightSavingsString(DateTime date)
-        {
-            // setting the hour to 20 so that dates will never change
-            var dateForString = new DateTime(
-                date.Year,
-                date.Month,
-                date.Day,
-                20,
-                date.Minute,
-                date.Second,
-                date.Millisecond);
-
-            var info = TimeZoneInfo.FindSystemTimeZoneById("W. Europe Standard Time");
-            var isDaylightSavingTime = info.IsDaylightSavingTime(dateForString);
-
-            var retVal = dateForString.ToString(
-                isDaylightSavingTime
-                    ? $"yyyy'-'MM'-'dd'T'22':'mm':'ss'Z'"
-                    : "yyyy'-'MM'-'dd'T'23':'mm':'ss'Z'",
-                CultureInfo.InvariantCulture);
-
-            return retVal;
         }
     }
 }
