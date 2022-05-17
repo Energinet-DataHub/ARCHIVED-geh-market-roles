2544f877eca6eb6c97c7c1d4b8c9ec62c8847f48
Kristian F. Thomsen <kft@energinet.dk>
2022-05-17 14:16:41 +0200

    Remove unused imports

--- source/Processing.Tests/Domain/BusinessProcesses/MoveIn/ConsumerMoveInTests.cs
+++ source/Processing.Tests/Domain/BusinessProcesses/MoveIn/ConsumerMoveInTests.cs
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-using System;
-using System.Linq;
 using NodaTime;
 using Processing.Domain.BusinessProcesses.MoveIn;
 using Processing.Domain.BusinessProcesses.MoveIn.Errors;
