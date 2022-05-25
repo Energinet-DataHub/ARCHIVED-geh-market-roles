85ecab19b18ba4183d50c3ed2c5a356d3387bc1a
Kristian F. Thomsen <kft@energinet.dk>
2022-05-25 09:51:24 +0200

    Add integration event mapper

---
+++ source/Processing.Infrastructure/Configuration/EventPublishing/IntegrationEventMapper.cs
@@ -0,0 +1,44 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Processing.Infrastructure.Configuration.EventPublishing
+{
+    public class IntegrationEventMapper
+    {
+        private readonly HashSet<EventMetadata> _values = new();
+
+        public void Add(string eventName, Type eventType, int version, string topicName)
+        {
+            _values.Add(new EventMetadata(eventName, eventType, version, topicName));
+        }
+
+        public EventMetadata? GetByName(string eventName)
+        {
+            return _values
+                .FirstOrDefault(metadata => metadata.EventName.Equals(eventName, StringComparison.OrdinalIgnoreCase));
+        }
+
+        public EventMetadata? GetByType(Type eventType)
+        {
+            return _values
+                .FirstOrDefault(metadata => metadata.EventType == eventType);
+        }
+    }
+
+    public record EventMetadata(string EventName, Type EventType, int Version, string TopicName);
+}
