ef2af117921b6e8741a8b1eaa1aa562983b7f24a
Kristian F. Thomsen <kft@energinet.dk>
2022-05-25 14:41:55 +0200

    Fix license line

--- source/Processing.IntegrationTests/TestDoubles/ServiceBusSenderFactoryStub.cs
+++ source/Processing.IntegrationTests/TestDoubles/ServiceBusSenderFactoryStub.cs
@@ -15,7 +15,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using Processing.Infrastructure.Configuration.EventPublishing;
 using Processing.Infrastructure.Configuration.EventPublishing.AzureServiceBus;
 
 namespace Processing.IntegrationTests.TestDoubles
