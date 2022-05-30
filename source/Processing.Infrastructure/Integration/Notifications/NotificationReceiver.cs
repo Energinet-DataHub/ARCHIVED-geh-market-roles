e7754c7515ec39d9dadbf7dc2e0d2db13152048a
BjarkeMeier <hej@bjarkemeier.dk>
2022-05-30 13:46:09 +0200

    No need to commit

--- source/Processing.Infrastructure/Integration/Notifications/NotificationReceiver.cs
+++ source/Processing.Infrastructure/Integration/Notifications/NotificationReceiver.cs
@@ -33,7 +33,6 @@ namespace Processing.Infrastructure.Integration.Notifications
         public async Task PublishAndCommitAsync(object notification)
         {
             await _mediator.Publish(notification).ConfigureAwait(false);
-            await _unitOfWork.CommitAsync().ConfigureAwait(false);
         }
     }
 }
