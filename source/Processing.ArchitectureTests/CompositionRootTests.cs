3422f23baa518bd61e6b018652b38d8b79db652f
Kristian F. Thomsen <kft@energinet.dk>
2022-05-31 09:52:00 +0200

    Add architecture test

---
+++ source/Processing.ArchitectureTests/CompositionRootTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Processing.Api;
+using Xunit;
+
+namespace Processing.ArchitectureTests;
+
+public class CompositionRootTests
+{
+    [Fact]
+    public void Ensure_registrations()
+    {
+        var program = new Program();
+        Environment.SetEnvironmentVariable("MARKET_DATA_DB_CONNECTION_STRING", "SomeString");
+        program.ConfigureApplication();
+
+        program.AssertConfiguration();
+    }
+}
