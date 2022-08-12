4dd33677ab858cd109014a2dc1cb060ffec970c2
Kristian F. Thomsen <kft@energinet.dk>
2022-08-12 11:38:56 +0200

    Logic must be handled in a separate transaction

---
+++ source/Messaging.Application/Transactions/MoveIn/CreateEndOfSupplyNotificationHandler.cs
@@ -0,0 +1,45 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Messaging.Domain.Transactions.MoveIn;
+
+namespace Messaging.Application.Transactions.MoveIn;
+
+public class CreateEndOfSupplyNotificationHandler : IRequestHandler<CreateEndOfSupplyNotification, Unit>
+{
+    private readonly IMoveInTransactionRepository _repository;
+    private readonly MoveInNotifications _notifications;
+
+    public CreateEndOfSupplyNotificationHandler(IMoveInTransactionRepository repository, MoveInNotifications notifications)
+    {
+        _repository = repository;
+        _notifications = notifications;
+    }
+
+    public Task<Unit> Handle(CreateEndOfSupplyNotification request, CancellationToken cancellationToken)
+    {
+        if (request == null) throw new ArgumentNullException(nameof(request));
+        var transaction = _repository.GetById(request.TransactionId);
+        if (transaction is null)
+        {
+            throw new TransactionNotFoundException(request.TransactionId);
+        }
+
+        return Unit.Task;
+    }
+}
