998318895c807df3bfc9ee1e2166b0efbb475c10
Daniel Pethjen <daniel@pethjen.dk>
2022-10-25 13:54:10 +0200

    get moveInProcess

--- source/Processing.Application/ChangeCustomerCharacteristics/ChangeCustomerCharacteristicsRequestHandler.cs
+++ source/Processing.Application/ChangeCustomerCharacteristics/ChangeCustomerCharacteristicsRequestHandler.cs
@@ -12,8 +12,10 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+using System;
 using System.Threading;
 using System.Threading.Tasks;
+using Dapper;
 using Processing.Application.Common;
 using Processing.Domain.MeteringPoints;
 
@@ -21,9 +23,38 @@ namespace Processing.Application.ChangeCustomerCharacteristics;
 
 public class ChangeCustomerCharacteristicsRequestHandler : IBusinessRequestHandler<ChangeCustomerCharacteristicsRequest>
 {
-    public Task<BusinessProcessResult> Handle(ChangeCustomerCharacteristicsRequest request, CancellationToken cancellationToken)
+    private readonly IDbConnectionFactory _dbConnectionFactory;
+
+    public ChangeCustomerCharacteristicsRequestHandler(IDbConnectionFactory dbConnectionFactory)
     {
-        var businessProcessId = BusinessProcessId.New();
-        return Task.FromResult(BusinessProcessResult.Ok(businessProcessId.Value.ToString()));
+        _dbConnectionFactory = dbConnectionFactory;
+    }
+
+    public async Task<BusinessProcessResult> Handle(ChangeCustomerCharacteristicsRequest request, CancellationToken cancellationToken)
+    {
+        if (request == null) throw new ArgumentNullException(nameof(request));
+
+        var moveInTransaction = await GetMoveInTransactionByProcessIdAsync(request.ProcessId)
+            .ConfigureAwait(false);
+
+        return BusinessProcessResult.Ok(request.ProcessId.Value.ToString());
+    }
+
+    private async Task<MoveInTransaction> GetMoveInTransactionByProcessIdAsync(BusinessProcessId processId)
+    {
+        var query = $"SELECT m.ProcessId AS {nameof(MoveInTransaction.ProcessId)}, " +
+                    $"m.CustomerMasterData AS {nameof(MoveInTransaction.CustomerMasterData)} " +
+                    $"FROM [b2b].MoveInTransactions m " +
+                    $"WHERE m.ProcessId = @ProcessId";
+
+        var dataModel = await _dbConnectionFactory.GetOpenConnection().QuerySingleOrDefaultAsync<MoveInTransaction>(
+            query,
+            new { ProcessId = processId.Value, }).ConfigureAwait(false);
+
+        return dataModel;
     }
 }
+
+public record MoveInTransaction(
+    string ProcessId,
+    string CustomerMasterData);
